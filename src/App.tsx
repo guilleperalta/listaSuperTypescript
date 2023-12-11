f4d579e8d986c7ba0238c383c461c5b47de6abef
guilleperalta <guille.peralta.dev@gmail.com>
2023-12-11 13:03:42 -0300

    Se agregan varias funciones, el ultimo agregado queda arriba de todo, se puede editar por ID y no por index, se puede agregar con precio y checked

--- src/App.tsx
+++ src/App.tsx
@@ -15,51 +15,66 @@ const App: React.FC = () => {
   const [price, setPrice] = useState(0)
   const [checked, setChecked] = useState(false)
   const [products, setProducts] = useState<Product[]>([])
+  const [productsOrderById, setProductsOrderById] = useState<Product[]>([])
   const [editIndex, setEditIndex] = useState<number | null>(null)
+  const [nextId, setNextId] = useState<number>(1)
   const inutNombre = useRef<HTMLInputElement>(null)
 
   useEffect(() => {
     const storedProducts = localStorage.getItem('products')
+    const storedNextId : string | null = localStorage.getItem('nextId')
+    storedNextId != null ? setNextId(parseInt(JSON.parse(storedNextId))) : setNextId(1)
     if (storedProducts) {
-      setProducts(JSON.parse(storedProducts))
+      const parsedProducts: Product[] = JSON.parse(storedProducts)
+      setProductsOrderById(parsedProducts.sort((a, b) => b.id - a.id))
+      setProducts(parsedProducts.sort((a, b) => b.id - a.id))
     }
   }, [])
 
   useEffect(() => {
     localStorage.setItem('products', JSON.stringify(products))
+    localStorage.setItem('nextId', JSON.stringify(nextId))
+    setProductsOrderById(products.sort((a, b) => b.id - a.id))
   }, [products])
 
   const handleAddProduct = () => {
     if (name && quantity > 0) {
       if (editIndex !== null) {
-        // Edit existing product
-        const updatedProducts = [...products]
-        updatedProducts[editIndex] = { id: editIndex, name, quantity, price , checked}
+        const updatedProducts = products.map(product => {
+          if (product.id === editIndex) {
+            return { ...product, name, quantity, price, checked }
+          }
+          return product
+        })
         setProducts(updatedProducts)
         setEditIndex(null)
       } else {
-        // Add new product
-        const newProduct: Product = { id: products.length, name, quantity, price , checked:false}
+        const newProduct: Product = { id: nextId, name, quantity, price, checked}
         setProducts([...products, newProduct])
+        setNextId(nextId + 1)
       }
-      // Clear input fields
       setName('')
       setQuantity(1)
       setPrice(0)
-      // Focus input
+      setChecked(false)
       if (inutNombre.current) {
         inutNombre.current.focus();
       }
     }
   }
 
-  const handleEditProduct = (index: number) => {
-    const productToEdit = products[index]
-    setName(productToEdit.name)
-    setQuantity(productToEdit.quantity)
-    setPrice(productToEdit.price)
-    setChecked(productToEdit.checked)
-    setEditIndex(index)
+  const handleEditProduct = (id: number) => {
+    const productToEdit: Product | undefined = products.find(product => product.id === id)
+    if (productToEdit != undefined) {
+      setName(productToEdit.name)
+      setQuantity(productToEdit.quantity)
+      setPrice(productToEdit.price)
+      setChecked(productToEdit.checked)
+      setEditIndex(productToEdit.id)
+    }
+    if (inutNombre.current) {
+      inutNombre.current.focus();
+    }
   }
 
   const handleDeleteProduct = (index: number) => {
@@ -90,7 +105,7 @@ const App: React.FC = () => {
   }
 
   const totalQuantity = products.reduce((sum, product) => sum + product.quantity, 0)
-  const totalPriceQuantity = products.reduce((sum, product) => sum + (product.price * product.quantity), 0).toFixed(2)
+  const totalPriceQuantity = products.reduce((sum, product) => sum + (product.checked ? (product.price * product.quantity) : 0) , 0).toFixed(2)
 
   return (
     <div className="mx-auto p-4 bg-gray-900 text-white w-screen h-screen overflow-y-auto pb-16">
@@ -107,7 +122,7 @@ const App: React.FC = () => {
           />
         </div>
         <div className="flex justify-between">
-          <div className="mb-4 w-full">
+          <div className="mb-4">
             <div className="flex justify-center items-center">
               <button onClick={handleDecreaseQuantity} className="bg-blue-500 text-white p-1 m-0 flex justify-center rounded-lg">
                 <i className="material-icons">remove</i>
@@ -124,9 +139,9 @@ const App: React.FC = () => {
               </button>
             </div>
           </div>
-          {editIndex !== null && (
+          {/* {editIndex !== null && (  */}
             <div className="mb-4 flex flex-row justify-center items-center w-full">
-              <span className="text-2xl">$</span>
+              <span className="text-2xl mr-1">$</span>
               <input
                 type="number"
                 value={price === 0 ? '' : price}
@@ -135,7 +150,14 @@ const App: React.FC = () => {
                 className="border rounded-lg p-2 text-center bg-gray-800 text-white no-spinners w-32"
               />
             </div>
-          )}
+          {/* )}  */}
+          <button className="mb-4 flex flex-row justify-center items-center" onClick={() => setChecked(!checked)}>
+            {checked ? (
+                <i className="material-icons text-green-400 text-4xl">check</i>
+              ) : (
+                <i className="material-icons text-gray-600 text-4xl">check</i>
+            )}
+          </button>
         </div>
         <button onClick={handleAddProduct} className="bg-green-700 text-white font-bold p-2 rounded-lg block w-full">
           {editIndex !== null ? 'ACTUALIZAR' : 'AGREGAR'}
@@ -155,7 +177,7 @@ const App: React.FC = () => {
               <div
                 key={index}
                 className="rounded-lg px-3 py-2 flex justify-between items-center bg-gray-800 cursor-pointer w-full"
-                onClick={() => handleEditProduct(index)}
+                onClick={() => handleEditProduct(product.id)}
               >
                 <div className="grid grid-cols-3 w-full">
                   <div className="text-left">{product.name}</div>
@@ -176,13 +198,13 @@ const App: React.FC = () => {
             </div>
           )
         ))}
-        {products.map((product, index) => (
+        {productsOrderById.map((product, index) => (
           product.checked && (
             <div key={index} className="flex justify-between">
               <div
                 key={index}
                 className="rounded-lg px-3 py-2 flex justify-between items-center bg-green-900 cursor-pointer w-full"
-                onClick={() => handleEditProduct(index)}
+                onClick={() => handleEditProduct(product.id)}
               >
                 <div className="grid grid-cols-3 w-full">
                   <div className="text-left">{product.name}</div>
@@ -204,8 +226,9 @@ const App: React.FC = () => {
           )
         ))}
         </div>
-        <div className="grid grid-cols-2 bg-gray-900 p-4 pr-12 fixed bottom-0 m-auto w-full md:w-2/3 lg:w-1/2 xl:w-1/3">
+        <div className="grid grid-cols-3 bg-gray-900 p-4 pr-12 fixed bottom-0 m-auto w-full md:w-2/3 lg:w-1/2 xl:w-1/3">
           <div className="text-left">Productos: {totalQuantity}</div>
+          <button className="text-center text-red-500 border border-red-500 rounded" onClick={()=>setProducts([])}>Eliminar todo</button>
           <div className="text-right">A pagar: $ {totalPriceQuantity}</div>
         </div>
       </div>
